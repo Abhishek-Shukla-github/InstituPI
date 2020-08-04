b72f1552169915edb4a47a4e65debf2bcf959bf1
Abhishek Shukla <abhishekshuklaratedr@gmail.com>
2020-08-04 23:54:38 +0530

    Set up Express route

--- server.js
+++ server.js
@@ -7,6 +7,29 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+app.get("/api/v1/bootcamps", (req, res) => {
+  res.status(200).json({ success: true, data: { msg: "Show all bootcamps" } });
+});
+app.post("/api/v1/bootcamps", (req, res) => {
+  res.status(200).json({ success: true, data: { msg: "Add a new bootcamp" } });
+});
+app.put("/api/v1/bootcamps/:id", (req, res) => {
+  res
+    .status(200)
+    .json({
+      success: true,
+      data: { msg: `Edit a bootcamp:- ${req.params.id}` },
+    });
+});
+app.delete("/api/v1/bootcamps/:id", (req, res) => {
+  res
+    .status(200)
+    .json({
+      success: true,
+      data: { msg: `Delete a bootcamp:- ${req.params.id}` },
+    });
+});
+
 app.listen(
   PORT,
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
