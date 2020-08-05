981778bc660196f6ab8a34e1fca17a92698bb932
Abhishek Shukla <abhishekshuklaratedr@gmail.com>
2020-08-05 15:47:15 +0530

    MongoDB setup completed

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 let express = require("express");
 let dotenv = require("dotenv");
+let colors = require("colors");
+const connectDB = require("./config/db");
 
 //Routes File
 let bootcamps = require("./routes/bootcamps");
 
 //Load the env vars
 dotenv.config({ path: "./config/config.env" });
+
+//Connect to database
+connectDB();
+
 const app = express();
 
 const PORT = process.env.PORT || 5000;
@@ -13,7 +19,16 @@ const PORT = process.env.PORT || 5000;
 //Mount Routers
 app.use("/api/v1/bootcamps", bootcamps);
 
-app.listen(
+const server = app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
 );
+
+//Handle unhandles promise Rejection
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error:- ${err.message}`.red);
+  //CLose the server $ exit the process
+  server.close(() => process.exit(1));
+});
