a301457997a4a36d7c5178c31891dfda18ba8c65
abhishek-shukla-github <abhishekshuklaratedr@gmail.com>
2021-03-09 23:53:53 +0530

    Add course average cost to the Bootcamp

--- routes/courses.js
+++ routes/courses.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const { getCourses, getCourse, addCourse } = require('../controllers/courses');
+const { getCourses, getCourse, addCourse, deleteCourse, updateCourse } = require('../controllers/courses');
 
 const router = express.Router({ mergeParams: true });
 
 router.route('/').get(getCourses).post(addCourse);
-router.route('/:id').get(getCourse);
+router.route('/:id').get(getCourse).delete(deleteCourse).put(updateCourse);
 
 module.exports = router;
